d99754fcdab06ded74d967e9479c19c66b3176a1
Your Name <your-email@gmail.com>
2026-04-24 13:28:38 +0530

    Added multiple code files

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-num = int(input('Enter a number: '))
-
-if num <= 1:
-    print('Not Prime')
-else:
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            print('Not Prime')
-            break
-    else:
-        print('Prime Number')
+print("Welcome to prime-number-checker")
+print("This is the main program file.")
